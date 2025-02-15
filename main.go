f4571a7f1d480f2cd707d2aa98e19d1c6a3c7ff7
vector <samcodebello@gmail.com>
2025-02-16 00:21:16 +0100

    added to test a fix

--- main.go
+++ main.go
@@ -33,7 +33,8 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("PORT environment variable is not set")
+		port = "8080"
+		// log.Fatal("PORT environment variable is not set")
 	}
 
 	apiCfg := apiConfig{}
@@ -90,7 +91,7 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		ReadTimeout: time.Hour,
+		ReadTimeout: time.Second * 5,
 		Addr:        ":" + port,
 		Handler:     router,
 	}
